b7905795d92f696a053dc9dbcadfd9f5a6820698
Tyler Mumford <tyler.mumford@icloud.com>
2022-04-25 01:11:14 -0600

    Add support for downloading Wells Fargo card records

--- Gathering/WellsFargoGatherer.cs
+++ Gathering/WellsFargoGatherer.cs
@@ -30,50 +30,103 @@ namespace BudgetReview.Gathering
 
             // Log in
             await page.GotoAsync(url);
-            await page.FillAsync("#j_username", username);
-            await page.FillAsync("#j_password", password);
-            await page.RunAndWaitForNavigationAsync(async () =>
-                await page.ClickAsync("button:text(\"Sign on\")")
-            , new PageRunAndWaitForNavigationOptions
-            {
-                UrlString = "https://connect.secure.wellsfargo.com/accounts/start",
-                WaitUntil = WaitUntilState.NetworkIdle,
-            });
+            await EnterUsernameAndPassword(page, username, password);
+            await page.ClickAsync("button:text(\"Sign on\")");
 
-            // Click the link for the card account (as opposed to the rewards account)
-            Log.Debug("MACU: Opening the export form");
-            await page.ClickAsync($"link:text(\"Card\")");
+
+            // Barrier #1: CAPTCHA
+            // -------------------
 
             if (await page.IsVisibleAsync("text=Help us make sure you're a person"))
             {
                 // CAPTCHA detected
+                Log.Warning("CAPTCHA detected! A person will need to help with this.");
+                await EnterUsernameAndPassword(page, username, password);
+                await page.FocusAsync("#nucaptcha-answer");
+
+                await page.WaitForNavigationAsync();
+            }
+            else
+            {
+                Log.Debug("No CAPTCHA detected.");
             }
 
-            // await page.ClickAsync("#export_trigger");
-            // var format = await page.WaitForSelectorAsync("#export-format-dropdown");
-            // if (format == null)
-            //     throw new Exception("Couldn't get the format dropdown");
+            // Barrier #2: Offer for checking account
+            // --------------------------------------
 
-            // // Fill out the form
-            // Log.Debug("MACU: Filling out the export form");
-            // await format.ClickAsync();
-            // await page.ClickAsync(".iris-list-item[data-value=\"54\"]");
+            if (await page.IsVisibleAsync("text=Simplify your life with Everyday Checking"))
+            {
+                Log.Warning("Offer for a checking account detected! Declining.");
+                await page.ClickAsync("button:text(\"No Thanks\")");
+            }
+            else
+            {
+                Log.Debug("No promotional offer detected.");
+            }
 
-            // await page.FillAsync("#Parameters_StartDate", DateRange.FirstDay.ToIso());
-            // await page.FillAsync("#Parameters_EndDate", DateRange.LastDay.ToIso());
+            // Okay, back on track.
+            // --------------------
 
-            // // Get the exported CSV file
-            // Log.Debug("MACU: Downloading");
-            // var download = await page.RunAndWaitForDownloadAsync(async () =>
-            //     await page.ClickAsync("#export_transactions_confirm_button")
-            // );
-            // var filename = $"Macu-{Guid.NewGuid()}.csv";
-            // await download.SaveAsAsync(filename);
+            // Click the link for the card account (as opposed to the rewards account)
+            await page.ClickAsync("[class*=AccountTitle]");
 
-            // await page.CloseAsync();
-            // Log.Information("MACU: Finished downloading");
-            // return filename;
-            return "NotQuiteImplementedYet";
+            await page.ClickAsync("text=Download Account Activity");
+            await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+
+            await page.ClickAsync("text=Comma Delimited");
+            // (This should happen before the dates are entered, otherwise their "Force=true" adjustment
+            // will cause the gatherer to try to fill them in before they're available.)
+
+            // NOTE: When I've done this manually, there's sometimes a modal with the text "No Internet connection."
+            // But I haven't seen this in any automated runs.
+
+            Log.Information($"Using first date like {DateRange.FirstDay.ToShortDateString()}");
+            var allowInvisible = new PageFillOptions { Force = true };
+            await WaitForDateFieldsToBeActionable(page);
+            await page.FillAsync("#fromDate", DateRange.FirstDay.ToShortDateString(), allowInvisible);
+            await page.FillAsync("#toDate", DateRange.LastDay.ToShortDateString(), allowInvisible);
+
+            var download = await page.RunAndWaitForDownloadAsync(async () =>
+            {
+                Log.Information("Clicking the Download button");
+                await page.ClickAsync("button:text('Download')");
+            });
+            var filename = $"WellsFargo-{Guid.NewGuid()}.csv";
+            await download.SaveAsAsync(filename);
+            Log.Information("Wells Fargo: Finished downloading");
+
+            await page.CloseAsync();
+            return filename;
+        }
+
+        // It sucks that a method like this seems required. Sheesh.
+        private async Task WaitForDateFieldsToBeActionable(IPage page)
+        {
+            // Wait for proxy element to be clickable
+            Log.Debug("summoning calendar popup");
+            await page.ClickAsync(".mwf-select-date-proxy");
+
+            // Determine what month name to look for
+            var preselectedDate = await page.InputValueAsync("#fromDate");
+            var date = DateTime.Parse(preselectedDate);
+            var month = date.ToString("MMMM");
+            var selector = $".mwf-calendar-month-label:text('{month}')";
+            Log.Debug("will wait for selector: " + selector);
+
+            // Wait for that month name (in the calendar popup) to appear
+            await page.WaitForSelectorAsync(selector);
+            Log.Debug("popup has appeared, dismissing...");
+
+            // Dismiss the calendar popup
+            await page.Keyboard.PressAsync("Escape");
+            await page.WaitForSelectorAsync(selector, new PageWaitForSelectorOptions { State = WaitForSelectorState.Hidden });
+            Log.Debug("popup is gone");
+        }
+
+        private async Task EnterUsernameAndPassword(IPage page, string username, string password)
+        {
+            await page.FillAsync("#j_username", username);
+            await page.FillAsync("#j_password", password);
         }
     }
 }
